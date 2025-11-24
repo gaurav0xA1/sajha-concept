d9283f3a45097ca71d41a246eb0bb425ca3f88f5
gaurav0xA1 <anupbhattarai46@gmail.com>
2025-11-24 09:40:27 +0545

    Fix app with correct components and Firebase auth

--- js/uielements.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById('menu-button');
-const panel = document.getElementById('togglepanel');
-
-menuBtn.addEventListener("click", function () {
-    console.log("Button clicked!");
-    panel.classList.toggle("open");
-});
